556060408c9609ff7b20616c1374d5697d6371a5
Bobson02 <global-m.bonheur@alustudent.com>
2024-11-20 18:19:32 +0200

    fix:adding filters to the code

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -7,15 +7,12 @@ def top_ten(subreddit):
     """Prints the titles of the first 10 hot posts for a given subreddit."""
     reddit_url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'User-agent': 'Mozilla/5.0'}
-
     # Make the request
     response = requests.get(reddit_url, headers=headers)
-
     if response.status_code == 200:
         data = response.json().get('data', {})
         posts = data.get('children', [])
         for post in posts[:10]:
             print(post['data']['title'])
     else:
-        # Return "OK" for invalid subreddit
         print("OK")
